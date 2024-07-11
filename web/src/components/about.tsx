4deae25ba35699e05ba6b2b1ec1da41d8c2788d0
Brendan Gray <brendan@alphasheep.co.za>
2024-07-11 20:16:28 +0200

    Expand the about section on the landing page

---
+++ web/src/components/about.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+import "./about.less";
+
+export default function About() {
+  return (
+    <div className="about prose">
+      <p>The scoring system used for the alternative rankings is as follows:</p>
+      <div className="code">
+        <div>accuracy = solved / attempted</div>
+        <div>time_used = time_in_seconds / time_available_in_seconds</div>
+
+        <div>score = solved * (accuracy) / sqrt(time_used)</div>
+      </div>
+      <p>
+        This system is specifically designed to balance rewarding larger
+        attempts, higher accuracy, and faster times. It was informed by a
+        detailed statistical analysis of nearly 20&nbsp;000 official results
+        over 17 years of competitions. The details of this analysis are
+        published in the following article:
+      </p>
+      <a
+        className="article-box"
+        href="https://bgray-dev.medium.com/multiblind-scoring-system-353c18a61dfe"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <h3 className="article-title">
+          Ranking Multiple Blindfolded Rubik's Cube Solves
+        </h3>
+        <h4 className="article-subtitle">
+          A data-driven proposal for a new system for ranking attempts at
+          solving multiple Rubik’s cubes blindfolded.
+        </h4>
+        <div className="article-details">32 min read · 4 Feb 2024</div>
+        <div className="article-link">bgray-dev.medium.com</div>
+      </a>
+
+      <p>
+        Should the WCA change to this system? No, please no. But it's a fun
+        exercise to explore.
+      </p>
+    </div>
+  );
+}
--- web/src/pages/home.tsx
+++ web/src/pages/home.tsx
@@ -2,60 +2,61 @@ import React from "react";
 import PageLayout from "../layouts/page-layout";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faExternalLinkAlt, faTrophy, faUser, faChartLine, faArrowDown19 } from "@fortawesome/free-solid-svg-icons";
+import {
+  faExternalLinkAlt,
+  faTrophy,
+  faUser,
+  faChartLine,
+  faArrowDown19,
+} from "@fortawesome/free-solid-svg-icons";
+import About from "../components/about";
 
-export default function HomePage () {
-  return <PageLayout>
-    <h1>Home</h1>
+export default function HomePage() {
+  return (
+    <PageLayout>
+      <h1>About</h1>
 
-    <p>
-      This is an unofficial alternative ranking system for the multiple blindfolded event. Results are based on those
-      of the <a href="https://www.worldcubeassociation.org/" target="_blank" rel="noreferrer">World Cube Association</a>.
-    </p>
-
-    <ul className="link-list">
-      <li>
-        <a href="/person" className="button-link">
-          <FontAwesomeIcon icon={faUser} />
-          <span>Person Results</span>
+      <p className="prose">
+        This is an unofficial alternative ranking system for the multiple
+        blindfolded event. Results are based on those of the{" "}
+        <a
+          href="https://www.worldcubeassociation.org/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          World Cube Association
         </a>
-      </li>
-      <li>
-        <a href="/competition" className="button-link">
-          <FontAwesomeIcon icon={faTrophy} />
-          <span>Competition Results</span>
-        </a>
-      </li>
-      <li>
-        <a href="/rankings" className="button-link">
-          <FontAwesomeIcon icon={faArrowDown19} />
-          <span>Alternative Rankings</span>
-        </a>
-      </li>
-      <li>
-        <a href="/records" className="button-link">
-          <FontAwesomeIcon icon={faChartLine} />
-          <span>Record History</span>
-        </a>
-      </li>
-    </ul>
+        .
+      </p>
 
-    <p>
-      The scoring system used for the alternative rankings is as follows:
-    </p>
+      <ul className="link-list">
+        <li>
+          <a href="/rankings" className="button-link">
+            <FontAwesomeIcon icon={faArrowDown19} />
+            <span>Alternative Rankings</span>
+          </a>
+        </li>
+        <li>
+          <a href="/records" className="button-link">
+            <FontAwesomeIcon icon={faChartLine} />
+            <span>Record History</span>
+          </a>
+        </li>
+        <li>
+          <a href="/person" className="button-link">
+            <FontAwesomeIcon icon={faUser} />
+            <span>Person Results</span>
+          </a>
+        </li>
+        <li>
+          <a href="/competition" className="button-link">
+            <FontAwesomeIcon icon={faTrophy} />
+            <span>Competition Results</span>
+          </a>
+        </li>
+      </ul>
 
-    <div className="code">
-      <div>accuracy = solved / attempted</div>
-      <div>time_used = time_in_seconds / time_available_in_seconds</div>
-
-      <div>score = solved * (accuracy) / sqrt(time_used)</div>
-    </div>
-
-    <p>
-      For detailed analysis and justification for this scoring system, please see <a
-        href="https://medium.com/@caring_lion_hedgehog_829/multiblind-scoring-system-353c18a61dfe" target="_blank" rel="noreferrer">
-        this article
-      </a>.
-    </p>
-  </PageLayout>;
-};
+      <About />
+    </PageLayout>
+  );
+}
