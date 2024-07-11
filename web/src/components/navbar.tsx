ac34ea99449175e54522389697f09346e85c61ac
Brendan Gray <brendan@alphasheep.co.za>
2024-07-11 20:14:57 +0200

    Improve navbar behaviour on mobile

--- web/src/components/navbar.tsx
+++ web/src/components/navbar.tsx
@@ -1,35 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHome, faTrophy, faUser, faChartLine, faArrowDown19 } from "@fortawesome/free-solid-svg-icons";
+import {
+  faTrophy,
+  faUser,
+  faChartLine,
+  faArrowDown19,
+  faBars,
+  faTimes,
+  faInfoCircle,
+} from "@fortawesome/free-solid-svg-icons";
 
 import "./navbar.less";
 
-export default function Navbar () {
-  return <>
-    <nav>
-      <ul>
-        <li>
-          <a href="/"><FontAwesomeIcon icon={faHome} /></a>
-          <div className="tooltip">Home</div>
-        </li>
-        <li>
-          <a href="/person"><FontAwesomeIcon icon={faUser} /></a>
-          <div className="tooltip">Person</div>
-        </li>
-        <li>
-          <a href="/competition"><FontAwesomeIcon icon={faTrophy} /></a>
-          <div className="tooltip">Competition</div>
-        </li>
-        <li>
-          <a href="/rankings"><FontAwesomeIcon icon={faArrowDown19} /></a>
-          <div className="tooltip">Rankings</div>
-        </li>
-        <li>
-          <a href="/records"><FontAwesomeIcon icon={faChartLine} /></a>
-          <div className="tooltip">Record History</div>
-        </li>
-      </ul>
-    </nav>
-  </>;
-};
+export default function Navbar() {
+  const [isNavCollapsed, setIsNavCollapsed] = useState<boolean>(true);
+
+  return (
+    <>
+      <nav>
+        <div className="nav-title">
+          <a href="/rankings">Alternative MBLD Ranks</a>
+          <button
+            className="nav-collapse-button"
+            onClick={() => setIsNavCollapsed(!isNavCollapsed)}
+          >
+            <FontAwesomeIcon icon={isNavCollapsed ? faBars : faTimes} />
+          </button>
+        </div>
+        <ul className={isNavCollapsed ? "nav-links collapsed" : "nav-links"}>
+          <li>
+            <a href="/rankings">
+              <FontAwesomeIcon icon={faArrowDown19} />
+              <span className="nav-label">Rankings</span>
+            </a>
+          </li>
+          <li>
+            <a href="/records">
+              <FontAwesomeIcon icon={faChartLine} />
+              <span className="nav-label">Records</span>
+            </a>
+          </li>
+          <li>
+            <a href="/person">
+              <FontAwesomeIcon icon={faUser} />
+              <span className="nav-label">Person</span>
+            </a>
+          </li>
+          <li>
+            <a href="/competition">
+              <FontAwesomeIcon icon={faTrophy} />
+              <span className="nav-label">Competition</span>
+            </a>
+          </li>
+          <li>
+            <a href="/">
+              <FontAwesomeIcon icon={faInfoCircle} />
+              <span className="nav-label">About</span>
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+}
